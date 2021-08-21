8e0d228e67f375fca45707dd43f82d1571359c37
dover247 <secondclass325@gmail.com>
2021-08-21 11:34:35 -0500

    aquasec site

--- app.py
+++ app.py
@@ -3,3 +3,4 @@ import requests
 print(requests.get("http://google.com"))
 print(requests.get("http://youtube.com"))
 print(requests.get("http://facebook.com"))
+print(requests.get("https://www.aquasec.com"))
