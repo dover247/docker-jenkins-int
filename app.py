219cf57e294d512197bdc5c364832365f3ecc858
dover247 <secondclass325@gmail.com>
2021-08-22 14:25:24 -0500

    commit

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ import requests
 print(requests.get("http://google.com"))
 print(requests.get("http://facebook.com"))
 print(requests.get("http://youtube.com"))
-print(requests.get("http://aquasec.com"))
+print(requests.get("http://aquasec.com").text)
